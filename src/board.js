be1422aac689873969a2f3bf213e1272d1151c10
Isaac Shoman and Kristen Chellis <kristen@>
2018-04-17 10:58:46 -0700

    writes tests to check row col and box check functins and then add row and col functions

--- src/board.js
+++ src/board.js
@@ -1,1 +1,36 @@
-export function Board() {}
+export function Board() {
+  this.board = [[], [], [], [], [], [], [], [], []];
+}
+
+Board.prototype.checkRow = function (row) {
+  for (var i = 0; i < 9; i++) {
+    var element1 = this.board[row][i];
+    for (var j = 0; j < 9; j++) {
+      var element2 = this.board[row][j];
+      if (element1 === element2 && i !== j) {
+        return false;
+      }
+    }
+  }
+  return true;
+};
+Board.prototype.checkCol = function (col) {
+  for (var i = 0; i < 9; i++) {
+    var element1 = this.board[i][col];
+    for (var j = 0; j < 9; j++) {
+      var element2 = this.board[j][col];
+      if (element1 === element2 && i !== j) {
+        return false;
+      }
+    }
+  }
+  return true;
+};
+// Board.prototype.checkBox = function (box) {
+//   var boxElements = [];
+//   for (var i = box[0]; i < (box[0] + 3); i++) {
+//     for (var j = box[1]; j < (box[1] + 3); j++) {
+//       boxElements.push(this.board[i][j])
+//     }
+//   }
+// };
